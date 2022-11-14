09db4e0d8a528624dd52c5e7443bb5d6fe36615a
quatbatieu <vuvantai894@gmail.com>
2022-11-14 19:22:06 +0700

    done

---
+++ src/modules/Hello/components/Hoe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hoe = () => {
+  return (
+    <div>
+        <h1>Hello !!</h1>
+    </div>
+  )
+}
+
+export default Hoe
---
+++ src/modules/Hello/pages/Hi.jsx
@@ -0,0 +1,12 @@
+import Hoe from "../components/Hoe";
+import React from 'react'
+
+const Hi = () => {
+  return (
+    <div>
+        <Hoe />
+    </div>
+  )
+}
+
+export default Hi
